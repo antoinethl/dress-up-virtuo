59e607cf97ad0398be551388f16fd5aaa8697553
Antoine THIOL <antoine.thiol@edu.devinci.fr>
2020-01-21 14:25:44 +0100

    Dress up virtuo final version with CSS, fields checks, date check and dynamic text + color for result

--- public/index.js
+++ public/index.js
@@ -6,11 +6,20 @@
     const fragment = document.createDocumentFragment();
     const div = document.createElement('div');
     const template = actors.map(actor => {
+
+      // I change the color of the text (red if have to pay and green if receive)
+      var color = "red"
+      var text = "will pay"
+
+      if (actor.type == "credit") {
+        text = "will receive"
+        color = "green"
+      }
       return `
         <div class="actor">
-          <span>${actor.who}</span>
-          <span>${actor.type}</span>
-          <span>${actor.amount}</span>
+          <span style="font-weight: bold; color:${color}">${actor.who}</span>
+          <span style="font-weight: bold; color:${color}">${text}</span>
+          <span style="font-weight: bold; color:${color}">${actor.amount} euros</span>
         </div>
       `;
     }).join('');
@@ -29,10 +38,27 @@
     const end = document.querySelector('#rental .js-end').value;
     const distance = document.querySelector('#rental .js-distance').value;
     const option = document.querySelector('#rental .js-option').checked;
-    const actors = VIRTUO.payActors(car, begin, end, distance, option);
 
-    render(actors);
+    // I check if all the fields are filled
+
+    if(car =="" || begin == "" || end == "" || distance == "") {
+      alert("Fill all fields please !")
+    }
+
+    // Then I check if the date is correct
+
+    else if (end < begin){
+      alert("Wrong date !")
+    }
+
+    // And now I can compute and print the result
+    else {
+      const actors = VIRTUO.payActors(car, begin, end, distance, option);
+      render(actors);
+    }
 
     return;
   });
 })();
+
+
